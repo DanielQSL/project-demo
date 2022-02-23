532f7dc76227cbd6b1ead48f4806143c640e1034
qianshuailong <shuailong.qian@weimob.com>
2022-02-23 14:57:44 +0800

    refactor:调整版本号

---
+++ project-base/src/main/java/com/qsl/project/base/exception/ServiceException.java
@@ -0,0 +1,28 @@
+package com.qsl.project.base.exception;
+
+/**
+ * 服务异常基类
+ *
+ * @author DanielQSL
+ */
+public class ServiceException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ServiceException() {
+        super();
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+
+}
