bee2b567c63b8a3165c68eabef84ec1d9332312d
DanielQSL <qiansl0316@gmail.com>
2025-10-08 03:54:49 +0800

    feat: pom

--- project-biz/src/main/java/com/qsl/project/ProjectApplication.java
+++ project-biz/src/main/java/com/qsl/project/ProjectApplication.java
@@ -1,8 +1,11 @@
 package com.qsl.project;
 
 import com.qsl.project.common.constants.ProjectConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 /**
  * 启动类
@@ -12,8 +15,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {ProjectConstants.BASE_PACKAGE})
 public class ProjectApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProjectApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(ProjectApplication.class, args);
+        SpringApplication app = new SpringApplication(ProjectApplication.class);
+        Environment env = app.run(args).getEnvironment();
+
+        LOGGER.info("\r----------------------------------------------------------\n" +
+                        "Application '{}' is running!\n" +
+                        "Profile(s):  {}\n" +
+                        "API Documentation:   http://{}:{}{}/doc.html\n" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getActiveProfiles(),
+                env.getProperty("server.address", "localhost"),
+                env.getProperty("server.port"),
+                env.getProperty("server.servlet.context-path", ""));
     }
 
 }
